1292fe118339ebeacd19acc8564f3026ffd30f6d
Dominic C <45153623+DominicChm@users.noreply.github.com>
2021-10-22 08:54:17 -0700

    Update MPU packet format

--- Sensor.js
+++ Sensor.js
@@ -1,11 +1,12 @@
 export default class Sensor {
     static getId() {
         throw new Error("NOT IMPLEMENTED");
-        return 0;
+        return 0;  //Here to trick Webstorm type checking.
     }
 
     static getDataLength() {
         throw new Error("NOT IMPLEMENTED");
+        return 0;  //Here to trick Webstorm type checking.
     }
 
     static parse(ArrayBuffer) {
