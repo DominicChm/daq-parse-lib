45297158c5babab97e13542414256974273947e8
Dominic C <45153623+DominicChm@users.noreply.github.com>
2021-11-04 13:53:06 -0700

    Add calibration for BP sensors

--- moduleTypes/SensorBrakePressure.js
+++ moduleTypes/SensorBrakePressure.js
@@ -1,6 +1,15 @@
 import Module from "../Module.js";
 
+
 export default class SensorBrakePressure extends Module {
+    constructor(config) {
+        super(config);
+        this.front_min_v = config.front_min_v
+        this.rear_min_v = config.rear_min_v
+        this.front_psi_per_v = config.front_psi_per_v
+        this.rear_psi_per_v = config.rear_psi_per_v
+    }
+
     static getTypeName() {
         return "brakePressure";
     }
@@ -9,10 +18,16 @@ export default class SensorBrakePressure extends Module {
         return 0x04;
     }
 
-    static parse(ArrayBuffer) {
+    parse(ArrayBuffer) {
+        const to_volts = (ain) => 3.3 * ain / 65535;
+        const voltages = {
+            front: to_volts(new DataView(ArrayBuffer).getUint16(0, true)),
+            rear: to_volts(new DataView(ArrayBuffer).getUint16(2, true)),
+        };
+
         return {
-            front: new DataView(ArrayBuffer).getUint16(0, true),
-            rear: new DataView(ArrayBuffer).getUint16(2, true),
+            front: (voltages.front - this.front_min_v) * this.front_psi_per_v,
+            rear: (voltages.rear - this.rear_min_v) * this.rear_psi_per_v,
         };
     }
 
